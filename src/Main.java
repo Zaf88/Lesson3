5e22baf8b69ec12edb0d60fdd4b3f19a5118380d
Your Name <applezafar@gmail.com>
2022-05-10 22:10:55 +1000

    Homework3

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int clientOS = 0;
+        if (clientOS == 1.0 || clientOS == 0);{
+            System.out.println("Follow the link to install IOS app");
+        int ClientOS = 1;
+        if (clientOS == 1);{
+            System.out.println("Follow the link to install Android app");
+            }
+        }
+    }
+}
