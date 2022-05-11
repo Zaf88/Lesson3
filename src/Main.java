01cdfc1f5577be106246b63f481a6e86ee9079f8
Your Name <applezafar@gmail.com>
2022-05-11 19:29:59 +1000

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,12 @@ public class Main {
             else if (clientOS == 1.0 || clientOS == 0) {
             }
             System.out.println("Install the light version of the IOS app from the link");
-            if (clientDeviceYear >= 2015) ;
-            else if (clientOS == 1.0 || clientOS == 0) ;
+            if (clientDeviceYear >= 2015);
+            else if (clientOS == 1.0 || clientOS == 0);
             {
             }
             System.out.println("Install the IOS app from the link");
-            if (clientDeviceYear < 2015) ;
+            if (clientDeviceYear < 2015);
             else if (clientOS == 1) {
             }
             System.out.println("Install the light version of the Android app from the link");
@@ -28,4 +28,19 @@ public class Main {
             if (clientDeviceYear >= 2015);
             else if (clientOS == 1); {
             }
-            }   System.out.println("Install the Android app from the link");}}
+            }   System.out.println("Install the Android app from the link");
+
+         int year = 2022;
+        if (year == year % 4);
+        else if (year != 100); {
+
+        }     System.out.println("The year is not bissextile");
+
+        int Year = 2020;
+        if (year == year % 4);
+        else if (year != 100); {
+
+        } System.out.println("The year is bissextily");
+    }
+}
+
