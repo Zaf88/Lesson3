c5fa26c9b2cd3ee4c3d426de9636055bbe52a98f
Your Name <applezafar@gmail.com>
2022-05-11 23:49:09 +1000

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,32 @@ public class Main {
             System.out.println("Follow the link to install Android app");
 
             int clientDeviceYear = 2015;
-            if (clientDeviceYear < 2015) ;
-            else if (clientOS == 1.0 || clientOS == 0) {
-            }
-            System.out.println("Install the light version of the IOS app from the link");
-            if (clientDeviceYear >= 2015) ;
-            else if (clientOS == 1.0 || clientOS == 0) ;
+            if (clientDeviceYear < 2015 && clientOS == 1.0 || clientOS == 0) ;
             {
             }
+            System.out.println("Install the light version of the IOS app from the link");
+            if (clientDeviceYear >= 2015 && clientOS == 1.0 || clientOS == 0) ;
+                        {
+            }
             System.out.println("Install the IOS app from the link");
-            if (clientDeviceYear < 2015) ;
-            else if (clientOS == 1) {
+            if (clientDeviceYear < 2015 && clientOS == 1);
+            {
             }
             System.out.println("Install the light version of the Android app from the link");
 
-            if (clientDeviceYear >= 2015) ;
-            else if (clientOS == 1) ;
-            {
+            if (clientDeviceYear >= 2015 && clientOS == 1) ;
+                        {
             }
         }
         System.out.println("Install the Android app from the link");
 
         int year = 2020;
-        if (year % 4 == 0 || year != 100) ;
+        if (year % 4 == 0 || year % 100 == 0 || year % 400 == 0) ;
 
-        System.out.println("The year is bissextile");
+        System.out.println("The year is leap Year");
          int Year = 2022;
-        if (year % 4!= 0 || year == 100);
-    System.out.println("The year is not bissextile");}}
+        if (year % 4!= 0 && year % 100 != 0 && year % 400 != 0);
+    System.out.println("The year is not Leap Year");}}
 
 
 
