6999350d302ae9b007fc2fb7679105519275c2ae
Your Name <applezafar@gmail.com>
2022-05-11 16:59:31 +1000

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,29 @@ public class Main {
 
     public static void main(String[] args) {
         int clientOS = 0;
-        if (clientOS == 1.0 || clientOS == 0);{
+        if (clientOS == 1.0 || clientOS == 0) ;
+        {
             System.out.println("Follow the link to install IOS app");
-        int ClientOS = 1;
-        if (clientOS == 1);{
+            int ClientOS = 1;
+            if (clientOS == 1) ;
             System.out.println("Follow the link to install Android app");
+
+            int clientDeviceYear = 2015;
+            if (clientDeviceYear < 2015) ;
+            else if (clientOS == 1.0 || clientOS == 0) {
             }
-        }
-    }
-}
+            System.out.println("Install the light version of the IOS app from the link");
+            if (clientDeviceYear >= 2015) ;
+            else if (clientOS == 1.0 || clientOS == 0) ;
+            {
+            }
+            System.out.println("Install the IOS app from the link");
+            if (clientDeviceYear < 2015) ;
+            else if (clientOS == 1) {
+            }
+            System.out.println("Install the light version of the Android app from the link");
+
+            if (clientDeviceYear >= 2015);
+            else if (clientOS == 1); {
+            }
+            }   System.out.println("Install the Android app from the link");}}
