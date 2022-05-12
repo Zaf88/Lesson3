90a4144f271092e4633bef2cd695a1fa29d88555
Your Name <applezafar@gmail.com>
2022-05-13 00:08:53 +1000

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -11,29 +13,47 @@ public class Main {
             System.out.println("Follow the link to install Android app");
 
             int clientDeviceYear = 2015;
-            if (clientDeviceYear < 2015 && clientOS == 1.0 || clientOS == 0);
+            if (clientDeviceYear < 2015 && clientOS == 1.0 || clientOS == 0) ;
             System.out.println("Install the light version of the IOS app from the link");
 
             if (clientDeviceYear >= 2015 && clientOS == 1.0 || clientOS == 0) ;
 
             System.out.println("Install the IOS app from the link");
-            if (clientDeviceYear < 2015 && clientOS == 1);
+            if (clientDeviceYear < 2015 && clientOS == 1) ;
             System.out.println("Install the light version of the Android app from the link");
 
-            if (clientDeviceYear >= 2015 && clientOS == 1);
-        System.out.println("Install the Android app from the link");
+            if (clientDeviceYear >= 2015 && clientOS == 1) ;
+            System.out.println("Install the Android app from the link");
 
-        int year = 2020;
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);}
+            int year = 2020;
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ;
+        }
 
         System.out.println("The year is leap Year");
 
-     int deliveryDistance = 95;
-     if (deliveryDistance <= 20);
-     System.out.println("DeliveryTime == 1 day");
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) ;
+        System.out.println("DeliveryTime == 1 day");
 
-     if (deliveryDistance == 20 || deliveryDistance == 60);
-     System.out.println("DeliveryTime == 1 day + 24 hours");
+        if (deliveryDistance == 20 || deliveryDistance == 60) ;
+        System.out.println("DeliveryTime == 1 day + 24 hours");
 
-    if (deliveryDistance == 60 || deliveryDistance == 100);
-    System.out.println("DeliveryTime == 1 day + 48 hours");}}
+        if (deliveryDistance == 60 || deliveryDistance == 100);
+        System.out.println("DeliveryTime == 1 day + 48 hours");
+
+        int monthNumber = (12);
+        switch (monthNumber) {
+            case 12,1,2:
+                System.out.println("Season is Winter");
+                break;
+            case 3,4,5:
+                System.out.println("Season is Spring");
+                break;
+            case 6,7,8:
+                System.out.println("Season is Summer");
+                break;
+            case 9,10,11:
+                System.out.println("Season is Autumn");
+                break;
+            case 13:}
+                System.out.println("Season is Undefined");}}
