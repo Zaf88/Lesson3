61b52343158c14af9f314ccc74ed22d34550a13d
Zaf88 <105032214+Zaf88@users.noreply.github.com>
2022-05-17 19:47:38 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,25 @@ public class Main {
 
     public static void main(String[] args) {
         int clientOS = 0;
-        if (clientOS == 1.0 || clientOS == 0) ;
+        if (clientOS == 1.0 || clientOS == 0)
         {
             System.out.println("Follow the link to install IOS app");
             int ClientOS = 1;
-            if (clientOS == 1) ;
-            System.out.println("Follow the link to install Android app");
+            if (clientOS == 1) {
+            System.out.println("Follow the link to install Android app");}
 
             int clientDeviceYear = 2015;
-            if (clientDeviceYear < 2015 && clientOS == 1.0 || clientOS == 0) ;
-            System.out.println("Install the light version of the IOS app from the link");
+            if (clientDeviceYear < 2015 && clientOS == 1.0 || clientOS == 0) {
+            System.out.println("Install the light version of the IOS app from the link");}
 
-            if (clientDeviceYear >= 2015 && clientOS == 1.0 || clientOS == 0) ;
+            if (clientDeviceYear >= 2015 && clientOS == 1.0 || clientOS == 0) {
 
-            System.out.println("Install the IOS app from the link");
-            if (clientDeviceYear < 2015 && clientOS == 1) ;
-            System.out.println("Install the light version of the Android app from the link");
+            System.out.println("Install the IOS app from the link");}
+            if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Install the light version of the Android app from the link");}
 
-            if (clientDeviceYear >= 2015 && clientOS == 1) ;
-            System.out.println("Install the Android app from the link");
+            if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Install the Android app from the link");}
 
             int year = 2020;
             if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ;
@@ -32,16 +32,16 @@ public class Main {
         System.out.println("The year is leap Year");
 
         int deliveryDistance = 95;
-        if (deliveryDistance <= 20) ;
-        System.out.println("DeliveryTime == 1 day");
+        if (deliveryDistance <= 20) {
+        System.out.println("DeliveryTime == 1 day");}
 
-        if (deliveryDistance == 20 || deliveryDistance == 60) ;
-        System.out.println("DeliveryTime == 1 day + 24 hours");
+        if (deliveryDistance == 20 || deliveryDistance == 60) {
+        System.out.println("DeliveryTime == 1 day + 24 hours");}
 
-        if (deliveryDistance == 60 || deliveryDistance == 100);
-        System.out.println("DeliveryTime == 1 day + 48 hours");
+        if (deliveryDistance == 60 || deliveryDistance == 100) {
+        System.out.println("DeliveryTime == 1 day + 48 hours"); }
 
-        int monthNumber = (12);
+        int monthNumber = 12;
         switch (monthNumber) {
             case 12,1,2:
                 System.out.println("Season is Winter");
@@ -55,5 +55,5 @@ public class Main {
             case 9,10,11:
                 System.out.println("Season is Autumn");
                 break;
-            case 13:}
+            default:}
                 System.out.println("Season is Undefined");}}
